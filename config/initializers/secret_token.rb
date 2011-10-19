dcaf338ef115d0f0518269b001d5e2ffdd648d58
Nikhil Goel <ngoel36@gmail.com>
2011-10-19 00:32:18 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nikhil::Application.config.secret_token = '9264f240aad63c220c7e6ac4afd6fdadd1ada536121b2cd755839d7eaadd1f44b87bc1f703c5647e5ac7291899f5c141c4673ac0e6d3b6d8c09dbb12a47c7eb7'
